ecd9b1b5c556939e7cc51d7bfbbdf5cf1caca7cb
NewPlayer <karasztamas@gmail.com>
2016-03-03 06:39:28 +0100

    module darabkák, logikai felbontás elkezdése

--- app/app.js
+++ app/app.js
@@ -1,5 +1,14 @@
 var program = require('spigot_manager_program');
+var config = require('spigot_manager_config');
+var spigot_manager = require('./spigot_manager');
 
-if (program.start) {
-    console.log('starting spigot server');
-}
+program
+		// Bind server handlers
+		.on('start, -start, --start', spigot_manager.start)
+		.on('stop', spigot_manager.stop)
+		.on('restart', spigot_manager.restart)
+		.on('status', spigot_manager.status)
+		.on('switch', spigot_manager.switch);
+
+// Parse arguments
+program.parse(process.argv);
---
+++ app/spigot_manager.js
@@ -0,0 +1,20 @@
+module.exports = new spigot_manager();
+
+function spigot_manager() {};
+
+
+spigot_manager.prototype.start = function(){
+	console.log('spigot manager start');
+};
+spigot_manager.prototype.stop = function(){
+	console.log('spigot manager start');
+};
+spigot_manager.prototype.restart = function(){
+	console.log('spigot manager start');
+};
+spigot_manager.prototype.status = function(){
+	console.log('spigot manager start');
+};
+spigot_manager.prototype.switch = function(){
+	console.log('spigot manager start');
+};
