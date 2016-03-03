ef6cb5f9b857b611a67a866e4f9419544924e06c
NewPlayer <karasztamas@gmail.com>
2016-03-03 22:28:18 +0100

    linux screen handler testing screen

--- app/app.js
+++ app/app.js
@@ -1,10 +1,14 @@
 var program = require('spigot_manager_program');
 var config = require('spigot_manager_config');
 var spigot_manager = require('./spigot_manager');
+var linux_screen = require('linux_screen');
+var screen = new linux_screen({
+	name: config.get('screen_name')
+});
 
 program
 		// Bind server handlers
-		.on('start, -start, --start', spigot_manager.start)
+		.on('start', spigot_manager.start)
 		.on('stop', spigot_manager.stop)
 		.on('restart', spigot_manager.restart)
 		.on('status', spigot_manager.status)
@@ -12,3 +16,5 @@ program
 
 // Parse arguments
 program.parse(process.argv);
+
+screen.exec('mc');
